951cfa61f48e191137d01d5e2a7fca7a6af7ad92
Black <black@ubuntu.ubuntu-domain>
2014-05-30 22:09:49 +0300

    Some work on the bugs

--- templates/link-wrapper.tpl.php
+++
@@ -1,3 +0,0 @@
-<div class="link-container">
-  <?php print $element['#children']; ?>
-</div>
--- templates/ordered-list.tpl.php
+++
@@ -1,9 +0,0 @@
-<h5>
-  <?php print t('Title:'); ?>
-  <?php print $title; ?>
-</h5>
-<ul>
-  <?php foreach ($items as $item) : ?>
-  <li><?php print $item; ?></li>
-  <?php endforeach; ?>
-</ul>
